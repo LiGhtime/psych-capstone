4566c2ffd954586b35da6a1bedeb002404bb2145
LiGhtime <35538966+LiGhtime@users.noreply.github.com>
2021-04-29 16:28:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var server = app.listen(process.env.PORT || 8080, function () {
 // app.get("/", function(req, res) {
 //   res.send('hello world')
 // })
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
     //res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
 //     res.sendFile(path.join(__dirname, '/auth/login/login.page.html'))
     res.sendFile(path.join(__dirname, '../index.html'))
