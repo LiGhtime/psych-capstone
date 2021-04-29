832a553f8821d298b257b04a363bafe6fa15acb1
LiGhtime <35538966+LiGhtime@users.noreply.github.com>
2021-04-28 20:43:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var app = express();
 //})
 app.get('/*', function(req,res) {
     //res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
-    res.sendFile(path.join(__dirname, '/front-end/src/', 'index.html'));
+    res.sendFile(path.join(__dirname, '/front-end/src', 'index.html'));
 })
 
 app.use(bodyParser.json());
