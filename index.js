4d2e4390dddb3f2fce63cd8fac901e1c49158853
LiGhtime <35538966+LiGhtime@users.noreply.github.com>
2021-04-29 17:40:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ var server = app.listen(process.env.PORT || 8080, function () {
 // })
 app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
-    res.sendFile(path.join(__dirname, '/auth/login/login.page.html'))
+    res.sendFile(path.join('psych-capstone/front-end/src/', __dirname, '/auth/login/login.page.html'))
 //     res.sendFile(path.join(__dirname, '../index.html'))
 //     var a = path.join(__dirname, '../index.html');
 //     res.send(`This is ${a}`)
